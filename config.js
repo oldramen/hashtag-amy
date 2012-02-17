e4157c23deea1c30f9e66db22080a6e139cc4902
yayramen <daltongore@gmail.com>
2012-02-17 00:35:17 +0000

    'added base files, with comments'

---
+++ config.js
@@ -0,0 +1,21 @@
+//This is where the party starts.
+//Requires src, which in turn requires any other external files
+
+//Variables to consider:
+//userid        =   userid of bot
+//roomid        =   roomid of room to be hosted in
+//owners        =   array of userids for owners
+//vips          =   array/object
+//maxsongs      =   default song limit
+//overmax       =   how many songs you can go over max before boot
+//waitsongs     =   how many songs you must wait after hitting limit to dj again
+//theme         =   default theme/genre for the room
+//limiton       =   if song limits are enabled by default
+//queueon       =   if queue is enabled by default
+//afk           =   default afk time (in minutes)
+//modbop        =   whether the /bop commands are for everyone or just mods
+//dodrink       =   whether bartender (if we add that function) is enabled by default
+//blabber       =   whether or not the bot talks without being prompted (greetings, dj announcements, etc.)
+//greeting      =   the default greeting message "Hey, {username}, welcome to {room}!"
+//helpmsg       =   the default help message. "Hey, {username}, {theme}, {songlimit}, {queue}, and {afk}."       
+//canadd        =   whether or not mods can add songs to bot's queue
---
+++ src.js
@@ -0,0 +1,3 @@
+//This is where everything happens
+//All of the main code will be here, and it will require
+//any external files.
