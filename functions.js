92ccef01b4d6b980fcd39a4b2ee48c427c32ef53
yayramen <daltongore@gmail.com>
2012-02-20 05:46:18 +0000

    'wrong var lolol'

--- functions.js
+++ functions.js
@@ -56,14 +56,14 @@ global.OnRemDJ = function(pData){
 };
 function LonelyDJ(spin) {
     if (spin == 'add') {
-        if (mDJs.length == 1) mbot.addDj();
-        if (mDJs.lenght >= 2 && mUserId in mDJs) mbot.remDj(mUserId);
+        if (mDJs.length == 1) mBot.addDj();
+        if (mDJs.lenght >= 2 && mUserId in mDJs) mBot.remDj(mUserId);
     }
     if (spin == 'rem') {
         if (mDJs.length == 1 && !(mUserId in mDJs)) {
-              mbot.addDj(); 
+              mBot.addDj(); 
         } else if (mUserId in mDJs) {
-            mbot.remDj(mUserId);
+            mBot.remDj(mUserId);
         }   
     }
 }
