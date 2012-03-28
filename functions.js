f257094b920d876dab0ec9c45badca1b8449a1df
Inumedia <inumedia@inumedia.net>
2012-03-28 17:51:45 -0500

    :D

--- functions.js
+++ functions.js
@@ -30,7 +30,9 @@ global.OnRegistered = function(pData){
 };
 
 global.OnDeregistered = function(pData){
-    for(var i = 0, len = pData.user.length; i < len; ++i) mUsers[pData.user[i].userid].Remove();   //not quite done yet. 
+    for(var i = 0, len = pData.user.length; i < len; ++i) 
+    	if(mUsers[pData.user[i].userid])
+    		mUsers[pData.user[i].userid].Remove();   //not quite done yet. 
     // pData.user[i].RecentlyLeft = Date.now();
     CalculateProperties();
 };
