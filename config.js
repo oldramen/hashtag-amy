0c102d4019fc2e8a34c2009b427db8a89d14cb36
yayramen <daltongore@gmail.com>
2012-05-03 19:37:23 -0700

    organization, such a situation

--- config.js
+++ config.js
@@ -4,59 +4,73 @@
     You can have multiple config files, and run multiple bots with the 
     same source files.
 */
-require("./enums.js");
+//Bring in the responses
+require("./enums.js"); //ignore this
 
-//This is where setup the streamers and shit for the party.
 
-//Variables
-global.mAuthId                  = "{auth_id}";      //authid of bot
-global.mUserId                  = "{user_id}";      //userid of bot
-global.mRoomId                  = "{room_id}";      //roomid of room to be hosted in
-global.mMongoHost               = "{mongo_host}";   //host of mongodb
+//Main Authorization Information
+global.mAuthId                  = "{auth_id}";//authid of bot
+global.mUserId                  = "{user_id}";//userid of bot
+global.mRoomId                  = "{room_id}";//roomid of room to be hosted in
+
+//Database Information
+global.mMongoHost               = "{mongo_host}";//host of mongodb, usually localhost or 127.0.0.1
 global.mMongoDatabase           = "{mongo_database}";//mongocollection for bot.
-global.mMongoUser               = "{mongo_user}";   //user of mongocollection
-global.mMongoPass               = "{mongo_pass}";   //password to mongocollection
-global.mMongoPort               = 27017;
-global.mName                    = "#Amy";           //name of bot
-global.mLaptop                  = "chrome";
-global.mTheme                   = "EDM";            //default theme/genre for the room
+global.mMongoUser               = "{mongo_user}";//user of mongocollection
+global.mMongoPass               = "{mongo_pass}";//password to mongocollection
+global.mMongoPort               = 27017;//Port, this is default
 
-global.mSpeakingLevel           = Speaking.Debug;   //whether or not the bot talks without being prompted (greetings, dj announcements, etc.)
+//General Information
+global.mName                    = "#Amy";//name of bot [if bot's name is different, this will change it.]
+global.mLaptop                  = "chrome";//Laptop the bot will use if the bot decides to DJ (lonely dj, /song)
+global.mTheme                   = "EDM";//default theme/genre for the room
 
-global.mOwners					= ['4e6498184fe7d042db021e95', '4f93ac70eb35c17529000585'];				//array of userids for who's an owner of the room.
-global.mVIPs					= [];				//array of userids for who's a VIP.
-global.mWhiteList				= [];				//array of userids for whitelist.
-global.mModerators				= [];
-global.mWhiteListEnabled		= false;			//whitelist is on ( true ) or off ( false )
-global.mMaxSongs                = 5;                //default song limit
-global.mWaitSongs               = 1;                //how many songs you must wait after hitting limit to dj again
-global.mLimitOn                 = true;             //if song limits are enabled by default
-global.mQueueOn                 = true;             //if queue is enabled by default
-global.mAFK                     = 15;               //default afk time (in minutes)
-global.mWarn                    = true;             //Whether bot warns for AFKs
-global.mMinAFKLimitOperator		= "&&";
-global.mMinUsersForAFKLimit		= 10;
-global.mMinDJsForAFKLimit		= 3;
-global.mMinSongLimitOperator    = null;;             //Here ya go Dalton!  :D
-global.mMinUsersForSongLimit    = null;             	//TODO: This.
-global.mMinDJsForSongLimit      = null;   	          	//
-global.mMinQueueOperator        = null;	            //
-global.mMinUsersForQueue        = null;	            //
-global.mMinDJsForQueue          = null;	            //
-global.mSongLimitUserProportion = 250;             //5 songs per 50 people in the room.
-global.mNoSpamTimeout           = 3;               	//3 seconds before the bot can say the same thing again.
-global.mLoopTimeout             = 5;               	//Does the main loop every 5 seconds.
-global.mSaveTimeout				= 30;				//Saves everyone after 30 seconds.
-global.mTimeForCacheFlush		= 15000;			//Time that the user stays in the cache after they leave.
+//User Arrays [ignore mModerators]
+global.mOwners					= ['4e6498184fe7d042db021e95', '4f93ac70eb35c17529000585'];//array of userids for who's an owner of the room.
+global.mVIPs					= [];//array of userids for who's a VIP.
+global.mWhiteList				= [];//array of userids for whitelist.
+global.mModerators				= [];//array of userids identified as moderators [dynamically generated, leave blank]
 
-global.mPMSpeak                 = true;            	//whether the bot will PM users so as to not spam
-global.mModBop                  = false;            //true, only mods can use the /dance command, false, anyone can.
-global.mWaiter                 	= true;            	//whether bartender (if we add that function) is enabled by default
-global.mBotDJ               	= false;           	//whether the bot can dj on command
-global.mLonelyDJ                = true;            	//whether the bot will dj if only one dj in room
-global.mCheckAFKWithLonely		= false;			//Should we check to see if people are afk when they're using Lonely DJ?
-global.mCheckSongCountWithLonely= false;			//Should we check the song count when they're using Lonely DJ?
-global.mAfkBop                	= true;            	//whether bops reset the afk timer or not
+
+//Song Limits
+global.mLimitOn                 = true;		//If songs limits are on by default
+global.mMaxSongs                = 5;		//How many songs you can play
+global.mWaitSongs               = 1;        //How many songs you must wait after hitting limit to dj again
+global.mMinSongLimitOperator    = null;     //Whether song limits are dynamically generated null by default
+global.mMinUsersForSongLimit    = null;     //How many users have to be in the room to auto turn on song limits
+global.mMinDJsForSongLimit      = null;   	//How many DJs have to be on deck to auto turn on song limits
+global.mSongLimitUserProportion = 250;      //5 songs per 50 people in the room.
+
+//Room Queue
+global.mQueueOn                 = true;     //If room queue is on by default
+global.mMinQueueOperator        = null;	    //Whether queue is turned on dynamically
+global.mMinUsersForQueue        = null;	    //How many users must be in the room to auto turn on the queue
+global.mMinDJsForQueue          = null;	    //How many DJs must be on deck to turn on queue automatically
+
+//AFK Settings
+global.mAFK                     = 15;       //How many minutes of AFK before you get booted
+global.mWarn                    = true;     //Should the bot warn users who are going afk?
+global.mMinAFKLimitOperator		= "&&";		//Whether or not the AFK Limit is generated dynamically
+global.mMinUsersForAFKLimit		= 10;		//Minimum users in the room before the AFK limit is turned on dynamically
+global.mMinDJsForAFKLimit		= 3;		//Minimum DJs on deck before AFK limit is turned on dynamically
+global.mAfkBop                	= true;     //Should bopping for a song reset that person's afk time?
+
+//Lonely DJ
+global.mLonelyDJ                = true;     //If true, bot will hop up to DJ if there is only one DJ [to prevent music cutoff]
+global.mCheckAFKWithLonely		= false;	//Should we check to see if people are afk when they're using Lonely DJ?
+global.mCheckSongCountWithLonely= false;	//Should we check the song count when they're using Lonely DJ?
+
+//General Toggles
+global.mWhiteListEnabled		= false;	//Whitelist is on ( true ) or off ( false )
+global.mPMSpeak                 = true;     //Whether the bot will PM users so as to not spam
+global.mModBop                  = false;    //True, only mods can use the /bop command, false, anyone can.
+global.mWaiter                 	= true;     //Whether waiter (/order) is enabled by default
+global.mBotDJ               	= false;    //Whether the bot can dj on command
+
+
+
+
+
 global.mAutoBopForMods			= true;				//If we should automatically bop for mods and above.
 global.mAutoBanBoots			= false;			//Instead of banning people, we simply boot them from the room.
 global.mAutoBanOnTTLink			= true;				//Autobans someone from the room if they give a link to another room and joined < 1 minute ago
@@ -68,59 +82,14 @@ global.mIgnoreSongCountOpenSpot = true;				//If the time that the spot was open 
 global.mIgnoreSongCountOnLonely = true;				//If LonelyDJ is currently being used, the person will be able to get up if this is true.
 global.mQueueGrabSpotTimeout	= 30;				//How many seconds we should hold a spot for someone when they are next in the queue and there is an open spot.
 
-global.mDefaultGreeting         = "/me hugs @{usernames}, welcome to {room}!"; //the default greeting message "Hey, {username}, welcome to {room}!"
-global.mVIPGreeting             = "Welcome @{usernames}, we have a VIP in the room!";
-global.mSuperGreeting           = "Hold the music! There's a SU in the house! Welcome, @{usernames}!";
-global.mModeratorGreeting       = "We've got a moderator in the room!  Welcome @{usernames}!";
-global.mAddDJ                   = null;//"Welcome to the deck, @{username}!  Remember to follow the rules, and the current theme is {theme}.";
-global.mRemDJ                   = null;
-global.mAddMod                  = null;
-global.mRemMod                  = null;
-global.mSnagMSg                 = null;
-global.mEndSong                 = "{songtitle}: {up} ↑, {down} ↓, {heartcount} <3.";
-global.mOverMaxSongsQueueOn     = "Hey, @{username}, you're over your max songs!  You've got to wait {songwait} song(s) to get back up.";
-global.mOverMaxSongsQueueOff    = null;
-global.mOverMaxSongsWarn	    = "Hey, @{username}, you've played your limit. Let someone else have a go.";
-global.mHelpMsg                 = "Hey, {username}, the theme is {theme}, the song limit is {songlimit}, The queue is currently {queuecurrentlyon}, and {afk} minutes for afk.";  //the default help message.      
-global.mWarnMsg                 = "Hey, @{username}, no falling asleep on deck!";  //the default warning message   
-global.mRemDJMsg                = "/tableflip {username}, you've been afk for too long.";
-global.mAdvanceQueue            = "Hey @{username}, it's your time to shine!  Please take your spot before "+mQueueGrabSpotTimeout+" seconds has passed.'";
-global.mWarnDJNotNextInQueue    = "Sorry, {username}, you have to wait your turn.  It's currently {nextinqueue}'s turn to get on deck.";
-global.mOpenSpotNoQueueing      = "Sorry, {username}, there's already an open spot.  Feel free to just hop up.";
-global.mQueueOff                = "I'm sorry but the queue is currently off.";
-global.mQueueStatus             = "There is currently {queueamount} people standing in line to get on deck.";
-global.mQueueEmpty              = "The queue is currently empty!";
-global.mQueueUsers              = "The queue is currently: {queueusers}, in that order.";
-global.mQueueAdded              = "Alright, {username}, you've been added to the queue!";
-global.mQueueAlreadyDJ          = "I'm sorry, {username}, but you're already a DJ."
-global.mAlreadyInQueue          = "Sorry, {username}, but you're already in the queue."
-global.mCommandsList            = "The list of commands are as follows: /{commands}";
-global.mThemeIs                 = "The current theme is {theme}.";
-global.mCurrentDJSongCount      = "The current song count is: {djsandsongcount}";
-global.mCurrentDJAfkCount       = "The current afk timer is: {djsandafkcount}";
-global.mRemoveFromQueue         = "You've been removed from the queue.";
-global.mNotInQueue              = "You're not in the queue.";
-global.mModRemoveFromQueue      = "Removed {username} from the queue.";
-global.mInfoOnRoom				= "Hi {username}! Welcome to {roomname}! The theme is {theme}, the song limit is {songlimit}, and if you're on deck you can only be afk for {afk} minutes.";
-global.mNotDJ					= "I'm sorry {username}, but you're not a dj.";
-global.mIsNowVIP				= "{username} is now a VIP.";
-global.mIsNoLongerVIP			= "{username} is no longer a VIP."
-global.mUnbanned				= "{username} is now unbanned.";
-global.mBanReason				= "You're banned.  Gtfo.";
-global.mBanned					= "{username} is now banned.";
-global.mTheirUserId				= "{username}'s userid is: {user.userid}";
-global.mYourUserId				= "Your userid is: {user.userid}";
-global.mNotOnWhiteList			= "I'm sorry, but you're not on the whitelist."; /// PMed to User.
-global.mAddedToWhiteList		= "{username} has been added to the whitelist.";
-global.mRemovedFromWhiteList    = "{username} has been removed from the whitelist.";
-global.mOffGenre				= "I'm sorry, but your song seems to be off genre, and we have to remove you from the deck."; // PMed to User.
-global.mPMWillBootOffDeck		= "Alright, I'll boot you off the deck at the end of your song.";
-global.mReadyRefresh			= "Okay, you can go ahead and refresh and I'll make sure you get your spot back.";
-global.mSpotOpenFor				= "The DJ Spot was open for {opentime} seconds. ";
-global.mHaveToWait				= "I'm sorry but you have to wait {user.mWaitingSongLimit} song(s) to get back up.";
 
-///global.mCanAdds                 = false;            //whether or not mods can add songs to bot's queue ///TODO: Why is this needed?
 
-//Now, we begin the party.
+//Advanced
+global.mSpeakingLevel           = Speaking.Debug;	//whether or not the bot talks without being prompted (greetings, dj announcements, etc.)
+global.mNoSpamTimeout           = 3;               	//3 seconds before the bot can say the same thing again.
+global.mLoopTimeout             = 5;               	//Does the main loop every 5 seconds.
+global.mSaveTimeout				= 30;				//Saves everyone after 30 seconds.
+global.mTimeForCacheFlush		= 15000;			//Time that the user stays in the cache after they leave.
 
+//Let's start the bot then.
 require("./main.js");
--- enums.js
+++ enums.js
@@ -3,57 +3,102 @@
     This is the enums file, where the speaking variables are stored.
     Change the value in the config files, controls how much the bot 
     spits out.
+
+    Common Variables: 
+    {usernames} - to spit out user's name in greeting
+    {username} - to spit out user's name everywhere else
+    {room} - to spit out room's name
 */
 
-///TODO: Add levels for Queue, Song Limit, and AFK.
+//Response Values
+
+//Greetings
+global.mDefaultGreeting         = "/me hugs @{usernames}, welcome to {room}!"; 
+global.mVIPGreeting             = "Welcome @{usernames}, we have a VIP in the room!";
+global.mSuperGreeting           = "Hold the music! There's a SU in the house! Welcome, @{usernames}!";
+global.mModeratorGreeting       = "We've got a moderator in the room!  Welcome @{usernames}!";
+global.mInfoOnRoom              = "Hi {username}! Welcome to {roomname}! The theme is {theme}, the song limit is {songlimit}, and if you're on deck you can only be afk for {afk} minutes.";
+//The mInfoOnRoom variable is the PM sent to first time visitors
+
+//Events
+global.mAddDJ                   = null;
+global.mRemDJ                   = null;
+global.mAddMod                  = null;
+global.mRemMod                  = null;
+global.mSnagMSg                 = null;
+global.mEndSong                 = "{songtitle}: {up} ↑, {down} ↓, {heartcount} <3.";
+
+//Song Limit
+global.mOverMaxSongsQueueOn     = "Hey, @{username}, you're over your max songs!  You've got to wait {songwait} song(s) to get back up.";
+global.mOverMaxSongsQueueOff    = null;
+global.mOverMaxSongsWarn        = "Hey, @{username}, you've played your limit. Let someone else have a go.";
+global.mHaveToWait              = "I'm sorry but you have to wait {user.mWaitingSongLimit} song(s) to get back up.";
+
+//AFK
+global.mWarnMsg                 = "Hey, @{username}, no falling asleep on deck!";  //the default warning message   
+global.mRemDJMsg                = "/tableflip {username}, you've been afk for too long.";
+
+//Queue
+global.mAdvanceQueue            = "Hey @{username}, it's your time to shine!  Please take your spot before "+mQueueGrabSpotTimeout+" seconds has passed.'";
+global.mWarnDJNotNextInQueue    = "Sorry, {username}, you have to wait your turn.  It's currently {nextinqueue}'s turn to get on deck.";
+global.mOpenSpotNoQueueing      = "Sorry, {username}, there's already an open spot.  Feel free to just hop up.";
+global.mQueueOff                = "I'm sorry but the queue is currently off.";
+global.mQueueStatus             = "There is currently {queueamount} people standing in line to get on deck.";
+global.mQueueEmpty              = "The queue is currently empty!";
+global.mQueueUsers              = "The queue is currently: {queueusers}, in that order.";
+global.mQueueAdded              = "Alright, {username}, you've been added to the queue!";
+global.mQueueAlreadyDJ          = "I'm sorry, {username}, but you're already a DJ."
+global.mAlreadyInQueue          = "Sorry, {username}, but you're already in the queue."
+global.mRemoveFromQueue         = "You've been removed from the queue.";
+global.mNotInQueue              = "You're not in the queue.";
+global.mModRemoveFromQueue      = "Removed {username} from the queue.";
+
+//VIP
+global.mIsNowVIP                = "{username} is now a VIP.";
+global.mIsNoLongerVIP           = "{username} is no longer a VIP."
+
+//Bans
+global.mUnbanned                = "{username} is now unbanned.";
+global.mBanReason               = "You're banned.  Gtfo.";
+global.mBanned                  = "{username} is now banned.";
+
+//Whitelist
+global.mNotOnWhiteList          = "I'm sorry, but you're not on the whitelist."; /// PMed to User.
+global.mAddedToWhiteList        = "{username} has been added to the whitelist.";
+global.mRemovedFromWhiteList    = "{username} has been removed from the whitelist.";
+
+//General
+global.mHelpMsg                 = "Hey, {username}, the theme is {theme}, the song limit is {songlimit}, The queue is currently {queuecurrentlyon}, and {afk} minutes for afk.";  //the default help message.      
+global.mCommandsList            = "The list of commands are as follows: /{commands}";
+global.mThemeIs                 = "The current theme is {theme}.";
+global.mCurrentDJSongCount      = "The current song count is: {djsandsongcount}";
+global.mCurrentDJAfkCount       = "The current afk timer is: {djsandafkcount}";
+global.mNotDJ                   = "I'm sorry {username}, but you're not a dj.";
+global.mTheirUserId             = "{username}'s userid is: {user.userid}";
+global.mYourUserId              = "Your userid is: {user.userid}";
+global.mOffGenre                = "I'm sorry, but your song seems to be off genre, and we have to remove you from the deck."; // PMed to User.
+global.mPMWillBootOffDeck       = "Alright, I'll boot you off the deck at the end of your song.";
+global.mReadyRefresh            = "Okay, you can go ahead and refresh and I'll make sure you get your spot back.";
+global.mSpotOpenFor             = "The DJ Spot was open for {opentime} seconds. ";
+
+
+//Speaking Values [advanced use only]
 global.SpeakingLevel = {
-    Misc: {
-        Val: 1,
-        status: "Misc"
-    },
-    Greeting:{
-        Val: 2,
-        status: "Greeting"
-    },
-    SongChange:{
-        val: 3,
-        status: "SongChange"
-    },
-    DJChange:{
-        val: 4,
-        status: "DJChange"
-    },
-    MODChange:{
-        val: 5,
-        status: "MODChange"
-    },
-    Errors: {
-        val: 6,
-        status: "Errors"
-    },
-    Debug: { 
-        val: 7,
-        status: "Debug"
-    },
-    Verbose: {
-        val: 8,
-        status: "Verbose"
-    }
+    Misc: {val: 1, status: "Misc"},
+    Greeting:{val: 2, status: "Greeting"},
+    SongChange:{val: 3, status: "SongChange"},
+    DJChange:{val: 4, status: "DJChange"},
+    MODChange:{val: 5, status: "MODChange"},
+    Errors: {val: 6, status: "Errors"},
+    Debug: { val: 7, status: "Debug"},
+    Verbose: {val: 8, status: "Verbose"}
 }
 
 global.Speaking = {
-    Default: { 
-        flags: [SpeakingLevel.Greeting, SpeakingLevel.Misc]
-    },
-    Shy: { 
-        flags: [SpeakingLevel.Misc]
-    },
-    Silent: { 
-        flags: []
-    },
-    Debug: {
-        flags: [SpeakingLevel.Verbose]
-    }
+    Default: { flags: [SpeakingLevel.Greeting, SpeakingLevel.Misc] },
+    Shy: { flags: [SpeakingLevel.Misc] },
+    Silent: { flags: [] },
+    Debug: { flags: [SpeakingLevel.Verbose] }
 };
 
 global.Requires = {
